c9029a6f8a738f5acff86e680d4fde9a2704b1fe
Björn Brandenburg <bbb@mpi-sws.org>
2019-09-25 19:54:27 +0200

    Initial arrival functions

---
+++ src/arrivals.rs
@@ -0,0 +1,251 @@
+use std::collections::VecDeque;
+
+type Time = u64;
+
+pub trait ArrivalBound {
+    fn number_arrivals(&self, delta: Time) -> u64;
+
+    fn maximal_trace(&self, horizon: Time) -> Vec<Time> {
+        let mut arrival_trace = Vec::new();
+
+        for delta in 1..=horizon {
+            let n = self.number_arrivals(delta);
+            while n as usize > arrival_trace.len() {
+                arrival_trace.push(delta - 1)
+            }
+        }
+
+        arrival_trace
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct Periodic {
+    pub period: Time,
+}
+
+fn divide_with_ceil(a: u64, b: u64) -> u64 {
+    a / b + if a % b > 0 { 1 } else { 0 }
+}
+
+impl ArrivalBound for Periodic {
+    fn number_arrivals(&self, delta: Time) -> u64 {
+        divide_with_ceil(delta, self.period)
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct Sporadic {
+    pub min_inter_arrival: Time,
+    pub jitter: Time,
+}
+
+impl ArrivalBound for Sporadic {
+
+    fn number_arrivals(&self, delta: Time) -> u64 {
+        if delta > 0 {
+            divide_with_ceil(delta + self.jitter, self.min_inter_arrival)
+        } else {
+            0
+        }
+    }
+}
+
+impl From<Periodic> for Sporadic {
+    fn from(p: Periodic) -> Self {
+        Sporadic {
+            min_inter_arrival: p.period,
+            jitter: 0,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+pub struct FiniteCurvePrefix {
+    min_distance: Vec<Time>,
+}
+
+impl FiniteCurvePrefix {
+    pub fn unroll_sporadic(s: &Sporadic, interval: Time) -> FiniteCurvePrefix {
+        let n = s.number_arrivals(interval) as usize + 1;
+        let mut v = Vec::with_capacity(n);
+        for i in 0..n {
+            let periods = i as u64 + 1;
+            if s.jitter >= periods * s.min_inter_arrival {
+                v.push(0)
+            } else {
+                v.push(periods * s.min_inter_arrival - s.jitter)
+            }
+        }
+        FiniteCurvePrefix { min_distance: v }
+    }
+
+    pub fn from_trace<'a>(
+        arrival_times: impl Iterator<Item = &'a Time>,
+        prefix_jobs: usize,
+    ) -> FiniteCurvePrefix {
+        let mut d = Vec::with_capacity(prefix_jobs);
+        let mut window: VecDeque<u64> = VecDeque::with_capacity(prefix_jobs + 1);
+
+        // consider all job arrivals in the trace
+        for t in arrival_times {
+            // sanity check: the arrival times must be monotonic
+            assert!(t >= window.back().unwrap_or(&t));
+            // look at all arrival times in the sliding window, in order
+            // from most recent to oldest
+            for (i, v) in window.iter().rev().enumerate() {
+                // compute the separation from the current arrival t to the arrival
+                // of the (i + 1)-th preceding job
+                let observed_gap = t - v;
+                if d.len() <= i {
+                    // we have not yet seen (i + 1) jobs in a row -> first sample
+                    d.push(observed_gap)
+                } else {
+                    // update belief if we have seen something of less separation
+                    // than previously
+                    d[i] = d[i].min(observed_gap)
+                }
+            }
+            // add arrival time to sliding window
+            window.push_back(*t);
+            // trim sliding window if necessary
+            if window.len() > prefix_jobs {
+                window.pop_front();
+            }
+        }
+        for (i, x) in d.iter().enumerate() {
+            println!("d[{}] = {}", i, x)
+        }
+
+        FiniteCurvePrefix { min_distance: d }
+    }
+
+    pub fn min_job_separation(&self) -> Time {
+        // minimum separation of two jobs given by first element
+        self.min_distance[0]
+    }
+
+    pub fn largest_known_distance(&self) -> Time {
+        *self.min_distance.last().unwrap()
+    }
+
+    pub fn jobs_in_largest_known_distance(&self) -> u64 {
+        self.min_distance.len() as u64
+    }
+
+    pub fn min_dinstance_of_jobs(&self, n: u64) -> Time {
+        match n {
+            0 => 0,
+            1 => 1,
+            _ => {
+                if n < self.min_distance.len() as u64 + 2 {
+                    self.min_distance[n as usize - 2]
+                } else {
+                    self.largest_known_distance() + 1
+                }
+            }
+        }
+    }
+
+    pub fn lookup_arrivals(&self, delta: Time) -> u64 {
+        // for really large vectors, this should be a binary search...
+        for (i, distance) in self.min_distance.iter().enumerate() {
+            if *distance + 1 > delta {
+                return (i as u64) + 1;
+            }
+        }
+        self.jobs_in_largest_known_distance() + 1
+    }
+}
+
+impl ArrivalBound for FiniteCurvePrefix {
+    fn number_arrivals(&self, delta: Time) -> u64 {
+        if delta > 0 {
+            // first, resolve long delta by super-additivity of arrival curves
+            let prefix = delta / self.largest_known_distance();
+            let prefix_jobs = prefix * self.jobs_in_largest_known_distance();
+            let tail = delta % self.largest_known_distance();
+            dbg!(delta);
+            dbg!(prefix);
+            dbg!(prefix_jobs);
+            dbg!(tail);
+            dbg!(self.min_job_separation());
+            if tail > self.min_job_separation() {
+                prefix_jobs + dbg!(self.lookup_arrivals(tail))
+            } else {
+                prefix_jobs + dbg!((tail > 0) as u64)
+            }
+        } else {
+            0
+        }
+    }
+}
+
+impl From<Periodic> for FiniteCurvePrefix {
+    fn from(p: Periodic) -> Self {
+        FiniteCurvePrefix {
+            min_distance: vec![p.period],
+        }
+    }
+}
+
+impl From<Sporadic> for FiniteCurvePrefix {
+    fn from(s: Sporadic) -> Self {
+        let jitter_jobs = divide_with_ceil(s.jitter, s.min_inter_arrival);
+        // Jitter can cause pessimism when applying super-additivity.
+        // Memory is cheap. Hence, unroll quite a bit to avoid running into
+        // pessimism too frequently.
+        // By default, unroll until the jitter jobs are no more than 10% of the
+        // jobs of the jobs in the unrolled interval, and until for at least 500 jobs.
+        let n = 500.max(jitter_jobs * 10);
+        FiniteCurvePrefix::unroll_sporadic(&s, n * s.min_inter_arrival)
+    }
+}
+
+#[derive(Copy, Clone, Debug)]
+pub struct Poisson {
+    pub rate: f64
+}
+
+impl Poisson {
+    pub fn arrival_probability(&self, delta: Time, njobs: u32) -> f64 {
+        // quick and dirty factorial: k!
+        let mut denominator = 1.0;
+        for x in 1..(njobs + 1) {
+            denominator *= x as f64;
+        }
+        let mean = delta as f64 * self.rate;
+        let mut numerator = (-mean).exp();    // e^(- rate * delta)
+        numerator *= mean.powi(njobs as i32); // (rate * delta)**k
+        numerator / denominator
+    }
+
+    pub fn approximate(&self, epsilon: f64) -> ApproximatedPoisson {
+        ApproximatedPoisson{ poisson: self.clone(), epsilon }
+    }
+}
+
+
+pub struct ApproximatedPoisson {
+    poisson: Poisson,
+    epsilon: f64
+}
+
+impl ArrivalBound for ApproximatedPoisson {
+    fn number_arrivals(&self, delta: Time) -> u64 {
+        if delta > 0 {
+            let mut cumulative_prob = 0.0;
+            let mut njobs = 0;
+            loop {
+                cumulative_prob += self.poisson.arrival_probability(delta, njobs);
+                if cumulative_prob + self.epsilon >= 1.0 {
+                    break
+                }
+                njobs += 1;
+            }
+            njobs as u64
+        } else {
+            0
+        }
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,182 @@
+pub mod arrivals;
+
+
+
+#[cfg(test)]
+mod tests {
+    use crate::arrivals::{self, ArrivalBound};
+    use assert_approx_eq::assert_approx_eq;
+
+    #[test]
+    fn periodic_arrivals() {
+        let a = arrivals::Periodic{ period: 10 };
+        assert_eq!(a.number_arrivals(0),  0);
+        assert_eq!(a.number_arrivals(1),  1);
+        assert_eq!(a.number_arrivals(8),  1);        
+        assert_eq!(a.number_arrivals(10),  1);
+        assert_eq!(a.number_arrivals(11),  2);
+        assert_eq!(a.number_arrivals(12),  2);
+        assert_eq!(a.number_arrivals(13),  2);
+        assert_eq!(a.number_arrivals(100), 10);
+        assert_eq!(a.number_arrivals(105), 11);
+    }
+
+    #[test]
+    fn periodic_arrivals_via_unroll_sporadic() {
+        let p = arrivals::Periodic{ period: 10 };
+        let s = arrivals::Sporadic::from(p);
+        let a = arrivals::FiniteCurvePrefix::unroll_sporadic(&s, 1000);
+        assert_eq!(a.number_arrivals(0),  0);
+        assert_eq!(a.number_arrivals(1),  1);
+        assert_eq!(a.number_arrivals(8),  1);        
+        assert_eq!(a.number_arrivals(10),  1);
+        assert_eq!(a.number_arrivals(11),  2);
+        assert_eq!(a.number_arrivals(12),  2);
+        assert_eq!(a.number_arrivals(13),  2);
+        assert_eq!(a.number_arrivals(100), 10);
+        assert_eq!(a.number_arrivals(101), 11);
+        assert_eq!(a.number_arrivals(105), 11);
+    }
+
+    #[test]
+    fn periodic_arrivals_via_sporadic() {
+        let p = arrivals::Periodic{ period: 10 };
+        let s = arrivals::Sporadic::from(p);
+        let a = arrivals::FiniteCurvePrefix::from(s);
+        assert_eq!(a.number_arrivals(0),  0);
+        assert_eq!(a.number_arrivals(1),  1);
+        assert_eq!(a.number_arrivals(8),  1);        
+        assert_eq!(a.number_arrivals(10),  1);
+        assert_eq!(a.number_arrivals(11),  2);
+        assert_eq!(a.number_arrivals(12),  2);
+        assert_eq!(a.number_arrivals(13),  2);
+        assert_eq!(a.number_arrivals(100), 10);
+        assert_eq!(a.number_arrivals(101), 11);
+        assert_eq!(a.number_arrivals(105), 11);
+    }
+    #[test]
+    fn periodic_arrivals_unrolled() {
+        let p = arrivals::Periodic{ period: 10 };
+        let a = arrivals::FiniteCurvePrefix::from(p);
+        assert_eq!(a.number_arrivals(0),  0);
+        assert_eq!(a.number_arrivals(1),  1);
+        assert_eq!(a.number_arrivals(8),  1);        
+        assert_eq!(a.number_arrivals(10),  1);
+        assert_eq!(a.number_arrivals(11),  2);
+        assert_eq!(a.number_arrivals(12),  2);
+        assert_eq!(a.number_arrivals(13),  2);
+        assert_eq!(a.number_arrivals(100), 10);
+        assert_eq!(a.number_arrivals(101), 11);
+        assert_eq!(a.number_arrivals(105), 11);
+    }
+
+    #[test]
+    fn periodic_arrivals_from_trace() {
+        let trace: Vec<u64> = vec![0, 10, 20, 30, 40];
+        let a = arrivals::FiniteCurvePrefix::from_trace(trace.iter(), 10);
+        assert_eq!(a.number_arrivals(0),  0);
+        assert_eq!(a.number_arrivals(1),  1);
+        assert_eq!(a.number_arrivals(8),  1);        
+        assert_eq!(a.number_arrivals(10),  1);
+        assert_eq!(a.number_arrivals(11),  2);
+        assert_eq!(a.number_arrivals(12),  2);
+        assert_eq!(a.number_arrivals(13),  2);
+        assert_eq!(a.number_arrivals(100), 10);
+        assert_eq!(a.number_arrivals(101), 11);
+        assert_eq!(a.number_arrivals(105), 11);
+    }
+
+
+    #[test]
+    fn compare_periodic_arrivals() {
+        let p = arrivals::Periodic{ period: 10 };
+        let s = arrivals::Sporadic::from(p);
+        let a = arrivals::FiniteCurvePrefix::from(p);
+        let b = arrivals::FiniteCurvePrefix::from(s);
+        let trace: Vec<u64> = vec![0, 10, 20, 30, 40];
+        let t = arrivals::FiniteCurvePrefix::from_trace(trace.iter(), 2);
+        for delta in 0..1000 {
+            assert_eq!(a.number_arrivals(delta),  p.number_arrivals(delta));
+            assert_eq!(s.number_arrivals(delta),  p.number_arrivals(delta));
+            assert_eq!(a.number_arrivals(delta),  b.number_arrivals(delta));
+            assert_eq!(p.number_arrivals(delta),  t.number_arrivals(delta));
+        }
+    }
+
+    #[test]
+    fn sporadic_arrivals() {
+        let a = arrivals::Sporadic{ min_inter_arrival: 10, jitter: 3 };
+        assert_eq!(a.number_arrivals(0),  0);
+        assert_eq!(a.number_arrivals(1),  1);
+        assert_eq!(a.number_arrivals(8),  2);
+        assert_eq!(a.number_arrivals(10),  2);
+        assert_eq!(a.number_arrivals(11),  2);
+        assert_eq!(a.number_arrivals(100), 11);
+        assert_eq!(a.number_arrivals(107), 11);
+        assert_eq!(a.number_arrivals(108), 12);
+        assert_eq!(a.number_arrivals(1108), 112);
+    }
+
+    #[test]
+    fn sporadic_arrivals_from_trace() {
+        let trace: Vec<u64> = vec![0, 7, 17, 27, 37, 47, 57, 67, 77, 87, 110, 117];
+        let a = arrivals::FiniteCurvePrefix::from_trace(trace.iter(), 5);
+        assert_eq!(a.number_arrivals(0),  0);
+        assert_eq!(a.number_arrivals(1),  1);
+        assert_eq!(a.number_arrivals(8),  2);
+        assert_eq!(a.number_arrivals(10),  2);
+        assert_eq!(a.number_arrivals(11),  2);
+    }
+
+    #[test]
+    fn sporadic_arrivals_unrolled() {
+        let s = arrivals::Sporadic{ min_inter_arrival: 10, jitter: 3 };
+        let a = arrivals::FiniteCurvePrefix::from(s);
+        assert_eq!(a.number_arrivals(0),  0);
+        assert_eq!(a.number_arrivals(1),  1);
+        assert_eq!(a.number_arrivals(8),  2);
+        assert_eq!(a.number_arrivals(10),  2);
+        assert_eq!(a.number_arrivals(11),  2);
+        assert_eq!(a.number_arrivals(100), 11);
+        assert_eq!(a.number_arrivals(107), 11);
+        assert_eq!(a.number_arrivals(108), 12);
+        assert_eq!(a.number_arrivals(1108), 112);
+    }
+
+    #[test]
+    fn sporadic_arrivals_large_jitter() {
+        let a = arrivals::Sporadic{ min_inter_arrival: 10, jitter: 16 };
+        assert_eq!(a.number_arrivals(0),  0);
+        assert_eq!(a.number_arrivals(1),  2);
+        assert_eq!(a.number_arrivals(4),  2);
+        assert_eq!(a.number_arrivals(5),  3);
+    }
+
+    #[test]
+    fn compare_sporadic_arrivals() {
+        let s = arrivals::Sporadic{ min_inter_arrival: 10, jitter: 16 };
+        let a = arrivals::FiniteCurvePrefix::from(s);
+        for delta in 0..1000 {
+            assert_eq!(a.number_arrivals(delta),  s.number_arrivals(delta));
+        }
+    }
+
+    #[test]
+    fn poisson() {
+        let p = arrivals::Poisson{ rate: 0.01 };
+        assert_approx_eq!(p.arrival_probability(100, 0), 0.368, 0.001);
+        assert_approx_eq!(p.arrival_probability(100, 1), 0.368, 0.001);
+        assert_approx_eq!(p.arrival_probability(100, 2), 0.184, 0.001);
+        assert_approx_eq!(p.arrival_probability(100, 3), 0.061, 0.001);
+        assert_approx_eq!(p.arrival_probability(100, 4), 0.015, 0.001);
+        assert_approx_eq!(p.arrival_probability(100, 5), 0.003, 0.001);
+        assert_approx_eq!(p.arrival_probability(100, 6), 0.0005, 0.001);
+
+        let a = p.approximate(0.0001);
+        let t = a.maximal_trace(2000);
+        for y in t {
+            println!("flood at {} [at most {} floods]", y, a.number_arrivals(y + 1));
+        }
+    }
+    
+}
