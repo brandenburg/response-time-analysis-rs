6b115954d91b03177640c3a11b5570d04adef185
Björn Brandenburg <bbb@mpi-sws.org>
2021-06-28 15:52:43 +0200

    introduce Service type alias

--- src/time.rs
+++ src/time.rs
@@ -1,7 +1,14 @@
-// We use a simple discrete time model
+/// This library uses a simple discrete time model.
 pub type Time = u64;
 
-// Syntactic sugar to give a hint as whether a time value denotes an interval length
-// or a specific point in time.
+/// Syntactic sugar to give a hint that a time value indicates a
+/// point in time or some offset.
 pub type Instant = Time;
+
+/// Syntactic sugar to give a hint that a time value denotes an
+/// interval length.
 pub type Duration = Time;
+
+/// Syntactic sugar to give a hint that a time value represents some
+/// amount of processor service.
+pub type Service = Time;
